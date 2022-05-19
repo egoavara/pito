283f0cf9672edd1ef753c251cba7e944fb989e74
김성현 <egoavara@gmail.com>
2022-05-19 11:08:35 +0900

    pito obj 버그픽스

--- src/derived-obj.ts
+++ src/derived-obj.ts
@@ -33,31 +33,38 @@ export function PitoObj<Properties extends Record<string, pito>, AdditionalPrope
         required: Object.keys(properties).filter(v => (properties[v] as any)['$optional'] !== true) as any,
         ...(option?.additionalProperties === true ? {} : { additionalProperties: false }),
         $wrap(data) {
-            for (const k in data) {
-                // @ts-ignore
-                if (this.required.indexOf(k) !== -1) {
-                    // @ts-ignore
-                    data[k] = this.properties[k].$wrap(data[k])
+            const result:any = {}
+            for (const k in this.properties) {
+                // @ts-expect-error
+                const isRequired = this.required.includes(k)
+                if (k in data) {
+                    // @ts-expect-error
+                    result[k] = this.properties[k].$wrap(data[k])
                 } else {
-                    // @ts-ignore
-                    if (this.properties[k] !== undefined) {
-                        // @ts-ignore
-                        data[k] = this.properties[k].$wrap(data[k])
+                    if (isRequired) {
+                        throw new Error(`not exist required property ${k}`)
                     }
-
                 }
             }
-            return data as any
+            return result
         },
         $unwrap(raw) {
-            for (const k in raw) {
-                // @ts-ignore
-                raw[k] = this.properties[k].$unwrap(raw[k])
+            const result:any = {}
+            for (const k in this.properties) {
+                // @ts-expect-error
+                const isRequired = this.required.includes(k)
+                if (k in raw) {
+                    // @ts-expect-error
+                    result[k] = this.properties[k].$wrap(raw[k])
+                } else {
+                    if (isRequired) {
+                        throw new Error(`not exist required property ${k}`)
+                    }
+                }
             }
-            return raw as any
+            return result
         },
         $strict() {
-
             return {
                 type: 'object',
                 properties: Object.fromEntries(
