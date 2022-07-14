2b117865558eb0c2ab2c4dee0c348d5c4af4a7b9
김성현 <egoavara@gmail.com>
2022-07-14 17:05:11 +0900

    ulit 약간 수정

--- src/union-lit.ts
+++ src/union-lit.ts
@@ -1,14 +1,16 @@
 import { pito, plugin } from "./pito.js";
 
 
-export type UnionLitSchema = { enums: (string | number)[] }
+export type UnionLitSchema = { type: 'string' | 'number', enums: (string | number)[] }
 
 export type PitoUnionLit<Lits extends string | number> = pito<Lits, Lits, UnionLitSchema, {}>;
 export const PitoUnionLit = <Lits extends [...(string | number)[]]>(...lits: Lits): PitoUnionLit<Lits[number]> => {
+    const temp = typeof lits[0] === 'string' ? 'string' : 'number'
     return {
+        type : temp,
         enums: lits,
         $strict() {
-            return { enums: lits }
+            return { type: temp, enums: lits }
         },
         $wrap(data) {
             return data
